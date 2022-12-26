518e0d4f4a03af895cae4392dfbf904bc34a126a
david <davidaren@gmail.com>
2022-12-26 20:21:57 +0100

    probando buildConfigField

--- app/src/main/java/com/appandroid/sagan/bicicadiz/activities/SplashActivity.kt
+++ app/src/main/java/com/appandroid/sagan/bicicadiz/activities/SplashActivity.kt
@@ -2,10 +2,9 @@ package com.appandroid.sagan.bicicadiz.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.appandroid.sagan.bicicadiz.R
-import com.appandroid.sagan.bicicadiz.databinding.ActivityMainBinding
+import com.appandroid.sagan.bicicadiz.BuildConfig
+
 import com.appandroid.sagan.bicicadiz.databinding.ActivityPantallaInicioBinding
 
 private lateinit var binding : ActivityPantallaInicioBinding
@@ -20,6 +19,10 @@ class SplashActivity : AppCompatActivity() {
         binding.animationView.playAnimation()
         binding.animationView.repeatCount = 10
         binding.animationView.alpha = 0.8F
+
+        if(BuildConfig.FLAVOR == "carrilBiciMalaga"){
+            binding.tvTitle2.text = BuildConfig.TITLE_SPLASH
+        }
 
         binding.btSiguiente.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
