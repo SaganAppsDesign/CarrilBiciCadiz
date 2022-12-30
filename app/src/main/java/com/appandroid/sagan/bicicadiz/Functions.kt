e576ef40300c7bf1b554c1e79ad63f18f0ac13c3
David Arenas <david.arenasaguirre@nttdata.com>
2022-12-30 16:44:55 +0100

    migración a Google Maps

--- app/src/main/java/com/appandroid/sagan/bicicadiz/Functions.kt
+++ app/src/main/java/com/appandroid/sagan/bicicadiz/Functions.kt
@@ -1,5 +1,7 @@
 package com.appandroid.sagan.bicicadiz
 
+import android.content.Context
+import android.graphics.Color
 import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,13 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.data.geojson.GeoJsonLayer
+import com.google.maps.android.data.geojson.GeoJsonPointStyle
 
 object Functions: AppCompatActivity() {
 
@@ -49,6 +58,74 @@ object Functions: AppCompatActivity() {
             override fun onAdOpened() {
                 // Code to be executed when an ad opens an overlay that
                 // covers the screen.
+            }
+        }
+    }
+
+    fun initMap(mMap: GoogleMap, googleMap: GoogleMap, binding: ActivityMainBinding, context: Context){
+
+        val cadiz = LatLng(36.517676, -6.276978)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cadiz))
+
+        googleMap.apply {
+            mapType = GoogleMap.MAP_TYPE_SATELLITE
+            moveCamera(CameraUpdateFactory.zoomTo(13F))
+            isTrafficEnabled = true
+            mMap.setPadding(0,150,0,150)
+            uiSettings.isZoomControlsEnabled = true
+            uiSettings.isCompassEnabled = true
+        }
+        loadCarrilBici(mMap, context)
+        loadAparcaBicis(mMap, context, binding)
+        loadFuentes(mMap, context, binding)
+    }
+
+    fun loadCarrilBici(mMap: GoogleMap, context: Context){
+        val carrilBici = GeoJsonLayer(mMap, R.raw.carril_bici, context)
+        val styleCarril = carrilBici.defaultLineStringStyle
+        styleCarril.color = Color.parseColor("#329221")
+        styleCarril.width = 15F
+        carrilBici.addLayerToMap()
+    }
+
+    fun loadAparcaBicis(mMap: GoogleMap, context: Context, binding: ActivityMainBinding){
+        val aparcabicis = GeoJsonLayer(mMap, R.raw.aparcabicis, context)
+
+        for (feature in aparcabicis.features) {
+            if (feature.getProperty("name") != null) {
+                val name = feature.getProperty("name")
+                val pointIcon = BitmapDescriptorFactory.fromResource(R.drawable.bicicleta)
+                val pointStyle = GeoJsonPointStyle()
+                pointStyle.icon = pointIcon
+                pointStyle.title = name
+                feature.pointStyle = pointStyle
+            }
+            binding.swAparcabicis.setOnCheckedChangeListener{_, isChecked ->
+                if (isChecked) {
+                    aparcabicis.addLayerToMap()
+                } else {
+                    aparcabicis.removeLayerFromMap()
+                }
+            }
+        }
+    }
+
+    fun loadFuentes(mMap: GoogleMap, context: Context, binding: ActivityMainBinding){
+        val fuentes = GeoJsonLayer(mMap, R.raw.fuentes, context)
+        for (feature in fuentes.features) {
+            if (feature.getProperty("type") != null) {
+                val pointIcon = BitmapDescriptorFactory.fromResource(R.drawable.fuente)
+                val pointStyle = GeoJsonPointStyle()
+                pointStyle.icon = pointIcon
+                pointStyle.title = "Fuente de agua potable"
+                feature.pointStyle = pointStyle
+            }
+        }
+        binding.swFuentes.setOnCheckedChangeListener{_, isChecked ->
+            if (isChecked) {
+                fuentes.addLayerToMap()
+            } else {
+                fuentes.removeLayerFromMap()
             }
         }
     }
--- app/src/main/java/com/appandroid/sagan/bicicadiz/activities/MainActivity.kt
+++ app/src/main/java/com/appandroid/sagan/bicicadiz/activities/MainActivity.kt
@@ -1,143 +1,68 @@
 package com.appandroid.sagan.bicicadiz.activities
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
-import android.graphics.Color
+import android.location.Location
 import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.animation.BounceInterpolator
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.appandroid.sagan.bicicadiz.ConnectionReceiver
-import com.appandroid.sagan.bicicadiz.Constants.APARCABICIS_GEO
-import com.appandroid.sagan.bicicadiz.Constants.APARCABICIS_ICON
-import com.appandroid.sagan.bicicadiz.Constants.CARRIL_BICI_GEO
-import com.appandroid.sagan.bicicadiz.Constants.CARRIL_ID
-import com.appandroid.sagan.bicicadiz.Constants.COLOR_BLANCO
-import com.appandroid.sagan.bicicadiz.Constants.FUENTES_GEO
-import com.appandroid.sagan.bicicadiz.Constants.FUENTES_ICON
-import com.appandroid.sagan.bicicadiz.Constants.FUENTES_ID
-import com.appandroid.sagan.bicicadiz.Constants.LAYER_FUENTES_ID
-import com.appandroid.sagan.bicicadiz.Constants.LAYER_ID
-import com.appandroid.sagan.bicicadiz.Constants.PARKING_ID
-import com.appandroid.sagan.bicicadiz.Constants.PARKING_LOCATION_NAME
+import com.appandroid.sagan.bicicadiz.Functions
 import com.appandroid.sagan.bicicadiz.Functions.loadAd
 import com.appandroid.sagan.bicicadiz.R
 import com.appandroid.sagan.bicicadiz.databinding.ActivityMainBinding
 import com.appandroid.sagan.bicicadiz.fragments.WelcomeInfoFragment
-import com.google.android.material.snackbar.BaseTransientBottomBar
-import com.google.android.material.snackbar.Snackbar
-import com.mapbox.android.core.permissions.PermissionsListener
-import com.mapbox.android.core.permissions.PermissionsManager
-import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.camera.CameraPosition
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
-import com.mapbox.mapboxsdk.geometry.LatLng
-import com.mapbox.mapboxsdk.location.LocationComponent
-import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
-import com.mapbox.mapboxsdk.location.LocationComponentOptions
-import com.mapbox.mapboxsdk.location.modes.CameraMode
-import com.mapbox.mapboxsdk.location.modes.RenderMode
-import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
-import com.mapbox.mapboxsdk.maps.Style
-import com.mapbox.mapboxsdk.maps.Style.*
-import com.mapbox.mapboxsdk.style.layers.LineLayer
-import com.mapbox.mapboxsdk.style.layers.Property
-import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
-import com.mapbox.mapboxsdk.style.layers.SymbolLayer
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
-import com.mapbox.pluginscalebar.ScaleBarOptions
-import com.mapbox.pluginscalebar.ScaleBarPlugin
-import java.io.IOException
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
-    private lateinit var binding : ActivityMainBinding
-    private var mapView: MapView? = null
-    private var mapboxMap: MapboxMap? = null
-    private var permissionsManager: PermissionsManager? = null
-    private var locationComponent: LocationComponent? = null
-    private lateinit var carrilBici: GeoJsonSource
-    private lateinit var parkingBicis: GeoJsonSource
-    private lateinit var fuentes: GeoJsonSource
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mMap: GoogleMap
+
     private var br: BroadcastReceiver = ConnectionReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        mapView = binding.mapView
-        mapView!!.onCreate(savedInstanceState)
-        mapView!!.getMapAsync(this)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        activeReceiver()
         val welcomeDialog = WelcomeInfoFragment()
         welcomeDialog.show(supportFragmentManager, "infoDialog")
         loadAd(binding)
+
+        activeReceiver()
     }
 
-    override fun onMapReady(mapboxMap: MapboxMap) {
-        this.mapboxMap = mapboxMap
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        enableMyLocation()
+        Functions.initMap(mMap, googleMap, binding, this)
+     }
 
-        loadMap(TRAFFIC_NIGHT)
-        mapboxMap.setMaxZoomPreference(18.0)
-        mapboxMap.setMinZoomPreference(12.0)
-
-        binding.zoomTolayer.setOnClickListener {
-             val position = CameraPosition.Builder()
-                    .target(LatLng(36.514444, -6.279378))
-                    .zoom(12.0)
-                    .tilt(0.0)
-                    .bearing(0.0)
-                    .build()
-            mapboxMap.animateCamera(CameraUpdateFactory
-                    .newCameraPosition(position), 5000)
-        }
-
-        binding.zoomTolocation.setOnClickListener{
-           locationComponent!!.cameraMode = CameraMode.TRACKING
-        }
-
-        mapboxMap.addOnMapClickListener { point ->
-            val screenPoint = mapboxMap.projection.toScreenLocation(point)
-            val features = mapboxMap.queryRenderedFeatures(screenPoint, LAYER_ID)
-            if (features.isNotEmpty()) {
-                val selectedFeature = features[0]
-                val title = selectedFeature.getStringProperty(PARKING_LOCATION_NAME)
-
-                if(title.isNullOrEmpty()){
-                    Toast.makeText(this, getString(R.string.estacionamiento_sin_nombre), Toast.LENGTH_SHORT).show()
-                    }
-                else {
-                    Snackbar.make(
-                        findViewById(R.id.activity_main),
-                        title,
-                        BaseTransientBottomBar.LENGTH_SHORT
-                    ).show()
-                   }
-            }
-            false
-        }
-
-        scaleBar(mapView, mapboxMap)
-    }
-
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {}
 
     @RequiresApi(Build.VERSION_CODES.P)
@@ -150,231 +75,83 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListene
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         val id = item.itemId
-        if (id == R.id.streets) {
-            loadMap(MAPBOX_STREETS)
-
+        if (id == R.id.normal) {
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
             return true
         }
-        if (id == R.id.satellite_streets) {
-            loadMap(SATELLITE_STREETS)
+        if (id == R.id.satelite) {
+            mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
             return true
         }
-        if (id == R.id.traffic) {
-            loadMap(TRAFFIC_NIGHT)
-            return true
+        if (id == R.id.terrain) {
+            mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
+           return true
         }
         return super.onOptionsItemSelected(item)
     }
 
-    override fun onExplanationNeeded(permissionsToExplain: List<String>) {}
-
-    override fun onPermissionResult(granted: Boolean) {
-        if (granted) {
-            mapboxMap!!.getStyle { style -> enableLocationComponent(style) }
-        } else {
-            Toast.makeText(this@MainActivity, getString(R.string.no_ubicacion), Toast.LENGTH_LONG).show()
-            finish()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        permissionsManager!!.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    private fun loadMap(base: String) {
-        mapboxMap!!.setStyle(base
-        ) { style ->
-            enableLocationComponent(style)
-            loadCarriles(style)
-            switchAparcaBicis(style)
-            switchFuentes(style)
-            if(binding.swAparcabicis.isChecked){
-                 loadAparcaBicis(style)
-            }
-            if(binding.swFuentes.isChecked){
-                loadFuentes(style)
-            }
-         }
-    }
-
-    private fun enableLocationComponent(style: Style) {
-        if (PermissionsManager.areLocationPermissionsGranted(this@MainActivity)) {
-            val locationComponentOptions = LocationComponentOptions.builder(this)
-                .pulseEnabled(true)
-                .pulseColor(Color.argb(255,159, 237, 254))
-                .pulseAlpha(.100f)
-                .pulseInterpolator(BounceInterpolator())
-                .build()
-
-            val locationComponentActivationOptions = LocationComponentActivationOptions
-                .builder(this, style)
-                .locationComponentOptions(locationComponentOptions)
-                .build()
-
-            locationComponent = mapboxMap!!.locationComponent
-            locationComponent!!.activateLocationComponent(locationComponentActivationOptions)
-
-            if (ActivityCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-                return
-            }
-            locationComponent!!.isLocationComponentEnabled = true
-            locationComponent!!.renderMode = RenderMode.COMPASS
-
-        } else {
-            permissionsManager = PermissionsManager(this@MainActivity)
-            permissionsManager!!.requestLocationPermissions(this@MainActivity)
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView!!.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        activeReceiver()
-        mapView!!.onResume()
-     }
-
-    override fun onPause() {
-        super.onPause()
-        mapView!!.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView!!.onStop()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView!!.onSaveInstanceState(outState)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView!!.onDestroy()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView!!.onLowMemory()
-    }
-
-    private fun loadJsonFromAsset(filename: String): String? {
-        return try {
-            val `is` = assets.open(filename)
-            val size = `is`.available()
-            val buffer = ByteArray(size)
-            `is`.read(buffer)
-            `is`.close()
-
-            String(buffer)
-
-        } catch (ex: IOException) {
-            ex.printStackTrace()
-            null
-        }
-    }
-
-    private fun loadCarriles(style: Style){
-
-        carrilBici = GeoJsonSource(CARRIL_ID, loadJsonFromAsset(CARRIL_BICI_GEO))
-
-        style.addSource(carrilBici)
-        style.addLayer(LineLayer("carril_background_layer", CARRIL_ID)
-            .withProperties(
-                lineCap(Property.LINE_CAP_SQUARE),
-                lineJoin(Property.LINE_JOIN_MITER),
-                lineOpacity(.7f),
-                lineWidth(8f),
-                lineColor(Color.parseColor(COLOR_BLANCO))
-            ))
-
-        style.addLayer(LineLayer("carril_layer", CARRIL_ID)
-            .withProperties(
-                lineCap(Property.LINE_CAP_SQUARE),
-                lineJoin(Property.LINE_JOIN_MITER),
-                lineOpacity(.7f),
-                lineWidth(4f),
-                lineColor(Color.parseColor("#329221"))
-            ))
-    }
-
-    private fun loadAparcaBicis(style: Style){
-        parkingBicis = GeoJsonSource(PARKING_ID, loadJsonFromAsset(APARCABICIS_GEO))
-        style.addSource(parkingBicis)
-        style.addImage(APARCABICIS_ICON, BitmapFactory.decodeResource(this.resources,
-            R.drawable.bicicleta
-        ))
-        val symbolLayer = SymbolLayer(LAYER_ID, PARKING_ID)
-        symbolLayer.withProperties(iconImage(APARCABICIS_ICON), iconAllowOverlap(false), iconSize(0.08f), iconIgnorePlacement(false))
-        style.addLayer(symbolLayer)
-    }
-
-    private fun loadFuentes(style: Style){
-        fuentes = GeoJsonSource(FUENTES_ID, loadJsonFromAsset(FUENTES_GEO))
-        style.addSource(fuentes)
-        style.addImage(FUENTES_ICON, BitmapFactory.decodeResource(this.resources,
-            R.drawable.fuente
-        ))
-        val symbolLayer = SymbolLayer(LAYER_FUENTES_ID, FUENTES_ID)
-        symbolLayer.withProperties(iconImage(FUENTES_ICON), iconAllowOverlap(false), iconSize(0.21f), iconIgnorePlacement(false))
-        style.addLayer(symbolLayer)
-    }
-
-    private fun switchAparcaBicis(style: Style){
-        binding.swAparcabicis.setOnCheckedChangeListener{_, isChecked ->
-            if (isChecked) {
-                loadAparcaBicis(style)
-                } else {
-                    if(style.layers.isNotEmpty()){
-                        style.removeLayer(LAYER_ID)
-                        style.removeSource(PARKING_ID)
-                    }
-               }
-        }
-    }
-
-    private fun switchFuentes(style: Style){
-        binding.swFuentes.setOnCheckedChangeListener{_, isChecked ->
-            if (isChecked) {
-                loadFuentes(style)
-            } else {
-                if(style.layers.isNotEmpty()){
-                    style.removeLayer(LAYER_FUENTES_ID)
-                    style.removeSource(FUENTES_ID)
-                }
-            }
-        }
-    }
-
-    private fun activeReceiver(){
+    private fun activeReceiver() {
         val networkIntentFilter = IntentFilter()
         networkIntentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
         registerReceiver(br, networkIntentFilter)
     }
 
-    private fun scaleBar(mapView: MapView?, mapBoxMap: MapboxMap){
-        val scaleBarPlugin = ScaleBarPlugin(mapView!!, mapBoxMap)
-        val scaleBarOptions = ScaleBarOptions(this)
-        scaleBarOptions
-            .setTextColor(R.color.colorAccent)
-            .setTextSize(30f)
-            .setBarHeight(15f)
-            .setBorderWidth(5f)
-            .setMetricUnit(true)
-            .setRefreshInterval(15)
-            .setMarginTop(30f)
-            .setMarginLeft(20f)
-            .setTextBarMargin(15f)
+    override fun onMyLocationButtonClick(): Boolean {
+        TODO("Not yet implemented")
+    }
 
-        scaleBarPlugin.create(scaleBarOptions)
+    override fun onMyLocationClick(p0: Location) {
+        TODO("Not yet implemented")
+    }
+
+    private fun isPermissionsGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (!::mMap.isInitialized) return
+        if (isPermissionsGranted()) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                mMap.isMyLocationEnabled = true
+            }else{
+                Toast.makeText(this, "Para activar la localización ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+        }
     }
 }
+
+
+
 
 
